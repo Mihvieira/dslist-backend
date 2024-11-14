f36bd430a184ab697b5cff2a3262aa1f377d7e2f
Mirelly <mih.vieira1@gmail.com>
2024-11-14 16:46:02 -0400

    get lists

---
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.repositories.GameListRepository;
+
+
+
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> list = gameListRepository.findAll();
+        return list.stream().map(x -> new GameListDTO(x)).collect(Collectors.toList());
+    }
+}
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,11 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+
+
 
 @Service
 public class GameService {
@@ -17,11 +20,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public GameDTO findById(Long id) {
         Game entity = gameRepository.findById(id).get();
         return new GameDTO(entity);
     }
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> list = gameRepository.findAll();
         return list.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
